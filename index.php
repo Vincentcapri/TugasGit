932968a11bdb5d88477fe55a17406def08971734
Vincentcapri <vincentcapri06101@gmail.com>
2022-11-23 20:11:33 +0700

    resolve conflict index.php

--- index.php
+++ index.php
@@ -4,12 +4,8 @@
     </head>
     <body>
         <h1>
-<<<<<<< HEAD
             Halaman Login
             Tambahan baris
-=======
-            Halaman Login pertama.
->>>>>>> fd8135b142220a31ebde32d3942ec6a00b6d4ae5
         </h1>
     </body>
 </html>
